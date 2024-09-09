295ac63ffae5ab8357c6cc4eabdf7100f649f935
Lucy Noonan <5f74n4m1@students.codeinstitute.net>
2024-09-09 12:56:31 +0000

    add const to choices and rules

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const rules = {
+    rock: ['scissors', 'lizard'],
+    paper: ['rock', 'spock'],
+    scissors: ['paper', 'lizard'],
+    lizard: ['paper', 'spock'],
+    spock: ['rock', 'scissors'],
+};
